daa70146f89bf4bd53c5b38a7cf8c710b3c4283b
Osvaldo Trujillo <osvaldo.trujillo.ingenieria@gmail.com>
2017-04-30 16:59:56 -0500

    Cambio de épocas y dimensiones

--- VGG/vgg.py
+++ VGG/vgg.py
@@ -9,38 +9,33 @@ from utils import reformatInput
 
 
 # dimensions of our images.
-img_width, img_height = 150, 150
-images = np.load('imagenes_1.npy')
-labels = np.load('labels_1.npy')
+images = np.load('imagenes_32.npy')
+labels = np.load('labels_32.npy')
 print labels.shape
-fold_pairs = np.load('fold_pairs_1.npy')
+fold_pairs = np.load('fold_pairs_32.npy')
 fold = fold_pairs[2]
 
 top_model_weights_path = 'bottleneck_fc_model.h5'
-epochs = 5
+epochs = 10
 batch_size = 32
 #input_tensor = Input(shape=(32,32,3))
-input_tensor = Input(shape=(150,150,3))
+input_tensor = Input(shape=(32,32,3))
 
 num_classes = len(np.unique(labels))
-#(X_train, y_train), (X_val, y_val), (X_test, y_test) = reformatInput(images, labels, fold)
-#X_train = X_train.astype("float32", casting='unsafe')
-#X_val = X_val.astype("float32", casting='unsafe')
-#X_test = X_test.astype("float32", casting='unsafe')
+(X_train, y_train), (X_val, y_val), (X_test, y_test) = reformatInput(images, labels, fold)
+X_train = X_train.astype("float32", casting='unsafe')
+X_val = X_val.astype("float32", casting='unsafe')
+X_test = X_test.astype("float32", casting='unsafe')
 
-X_train = images[:6000]
-X_val = images[6000:8000]
-y_train = labels[:6000]
-y_val = labels[6000:8000]
 
 images = np.swapaxes(images,0,2)
 X_train = np.swapaxes(X_train,1,3)
 X_val = np.swapaxes(X_val,1,3)
-#X_test = np.swapaxes(X_test,1,3)
+X_test = np.swapaxes(X_test,1,3)
 
 
 # build the VGG16 network
-base_model = applications.VGG16(weights='None',
+base_model = applications.VGG16(weights=None,
                                 include_top=False,
                                 input_tensor=input_tensor)
 
@@ -73,16 +68,16 @@ for layer in model.layers[:25]:
 # compile the model with a SGD/momentum optimizer
 # and a very slow learning rate.
 model.compile(loss='binary_crossentropy',
-              optimizer=optimizers.SGD(lr=1e-3, momentum=0.9),
+              optimizer=optimizers.SGD(lr=1e-5, momentum=0.9),
               metrics=['accuracy'])
 
 # fine-tune the model
-# model.fit(X_train, y_train, validation_data=(X_train, y_train), epochs=epochs)
+model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=epochs)
 
 #X_all = np.concatenate((X_train, X_val, X_test))
 #y_all = np.concatenate((y_train, y_val, y_test))
 
-model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=5)
+#model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=5)
 
 print('Done!')
 
