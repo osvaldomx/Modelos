a6128529d25cba33edcaa01ea1208aa48c24035d
Osvaldo Trujillo <osvaldo.trujillo.ingenieria@gmail.com>
2017-05-08 17:08:47 -0500

    CNN corriendo con datos p300 Parámetros: epochs = 10 acc = 0.79 auc=0.61 (Balanceo de pesos{0:0.2, 1:0.8})

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -1,7 +1,7 @@
 # coding: UTF-8
 import numpy as np
 import scipy.io as sio
-import tensorflow as tf
+import matplotlib.pyplot as plt
 
 from scipy.interpolate import griddata
 
@@ -16,7 +16,7 @@ from keras.utils import to_categorical
 from utils import reformatInput, cart2sph, augment_EEG, pol2cart
 
 from sklearn.preprocessing import scale
-
+from sklearn.metrics import auc, roc_curve
 
 np.random.seed(1234)
 
@@ -59,12 +59,12 @@ def gen_images(locs, features, n_gridpoints, normalize=True,
                         images.
     """
     feat_array_temp = []
-    nElectrodes = locs.shape[0]     # Number of electrodes
+    nElectrodes = locs.shape[0]  # Number of electrodes
     # Test whether the feature vector length is divisible by number of electrodes
     # assert features.shape[1] % nElectrodes == 0
     n_colors = int(features.shape[1] / nElectrodes)
     for c in range(n_colors):
-        feat_array_temp.append(features[:, c * nElectrodes : nElectrodes * (c+1)])
+        feat_array_temp.append(features[:, c * nElectrodes: nElectrodes * (c + 1)])
     if augment:
         if pca:
             for c in range(n_colors):
@@ -75,8 +75,8 @@ def gen_images(locs, features, n_gridpoints, normalize=True,
     nSamples = features.shape[0]
     # Interpolate the values
     grid_x, grid_y = np.mgrid[
-                     min(locs[:, 0]):max(locs[:, 0]):n_gridpoints*1j,
-                     min(locs[:, 1]):max(locs[:, 1]):n_gridpoints*1j
+                     min(locs[:, 0]):max(locs[:, 0]):n_gridpoints * 1j,
+                     min(locs[:, 1]):max(locs[:, 1]):n_gridpoints * 1j
                      ]
     temp_interp = []
     for c in range(n_colors):
@@ -85,26 +85,26 @@ def gen_images(locs, features, n_gridpoints, normalize=True,
     if edgeless:
         min_x, min_y = np.min(locs, axis=0)
         max_x, max_y = np.max(locs, axis=0)
-        locs = np.append(locs, np.array([[min_x, min_y], [min_x, max_y],[max_x, min_y],[max_x, max_y]]),axis=0)
+        locs = np.append(locs, np.array([[min_x, min_y], [min_x, max_y], [max_x, min_y], [max_x, max_y]]), axis=0)
         for c in range(n_colors):
             feat_array_temp[c] = np.append(feat_array_temp[c], np.zeros((nSamples, 4)), axis=1)
     # Interpolating
     for i in xrange(nSamples):
         for c in range(n_colors):
             temp_interp[c][i, :, :] = griddata(locs, feat_array_temp[c][i, :], (grid_x, grid_y),
-                                    method='cubic', fill_value=np.nan)
-        print('Interpolating {0}/{1}\r').format(i+1, nSamples, end='\r')
+                                               method='cubic', fill_value=np.nan)
+        print'Interpolating {0}/{1}\r'.format(i + 1, nSamples, end='\r')
     # Normalizing
     for c in range(n_colors):
         if normalize:
             temp_interp[c][~np.isnan(temp_interp[c])] = \
                 scale(temp_interp[c][~np.isnan(temp_interp[c])])
         temp_interp[c] = np.nan_to_num(temp_interp[c])
-    return np.swapaxes(np.asarray(temp_interp), 0, 1)     # swap axes to have [samples, colors, W, H]
+    return np.swapaxes(np.asarray(temp_interp), 0, 1)  # swap axes to have [samples, colors, W, H]
 
 
 if __name__ == '__main__':
-# Load electrode locations
+    # Load electrode locations
     print('Loading data...')
     locs_3d = sio.loadmat('p300_data/Neuroscan_locs_orig.mat')['A']
     locs_2d = []
@@ -112,10 +112,8 @@ if __name__ == '__main__':
     for e in locs_3d:
         locs_2d.append(azim_proj(e))
 
-    #feats = np.load('data_ACS.npy')
     feats = np.load('p300_data/data_ACS.npy')
 
-    #subj_nums = np.squeeze(sio.loadmat('sample_data/trials_subNums.mat')['subjectNum'])
     subj_nums = np.load('p300_data/subj.npy')
 
     # Leave-Subject-Out cross validation
@@ -135,46 +133,38 @@ if __name__ == '__main__':
 
     num_classes = len(np.unique(labels))
 
-
     # CNN Mode
     imsize = 32
     # Find the average response over time windows
-    #av_feats = reduce(lambda x, y: x + y, [feats[:, i * 206:(i + 1) * 206] for i in range(feats.shape[1] / 206)])
-    #av_feats = av_feats / (feats.shape[1] / 206)
-    av_feats = reduce(lambda x, y: x+y, [feats[:, i*206:(i+1)*206] for i in range(feats.shape[1] / 206)])
+    av_feats = reduce(lambda x, y: x + y, [feats[:, i * 206:(i + 1) * 206] for i in range(feats.shape[1] / 206)])
     av_feats = av_feats / (feats.shape[1] / 206)
-    with tf.device('/gpu:0'):
-        images = gen_images(np.array(locs_2d), av_feats, imsize, normalize=False)
+
+    # images = gen_images(np.array(locs_2d), av_feats, imsize, normalize=False)
+    images = np.load('images.npy')
+    # np.save('images', images)
+
     (X_train, y_train), (X_val, y_val), (X_test, y_test) = reformatInput(images, labels, fold)
-    # (X_train, y_train), (X_val, y_val), (X_test, y_test) = reformatInputIdx(av_feats, labels, fold)
     X_train = X_train.astype("float32", casting='unsafe')
     X_val = X_val.astype("float32", casting='unsafe')
     X_test = X_test.astype("float32", casting='unsafe')
-
-    #print('Generating training images...')
-    #X_train = gen_images(np.array(locs_2d), X_train, imsize, normalize=False)
-    #print('Genarating validation images...')
-    #X_val = gen_images(np.array(locs_2d), X_val, imsize, normalize=False)
-    #print('\n')
-
 
     print('Building model...')
 
     network = Sequential()
 
-    network.add(InputLayer(input_shape=(3,imsize,imsize)))
+    network.add(InputLayer(input_shape=(3, imsize, imsize)))
 
     network.add(Conv2D(32, 3, padding='same', data_format='channels_first'))
     network.add(Conv2D(32, 3, padding='same', data_format='channels_first'))
     network.add(Conv2D(32, 3, padding='same', data_format='channels_first'))
     network.add(Conv2D(32, 3, padding='same', data_format='channels_first'))
 
-    network.add(MaxPooling2D(pool_size=(2,2)))
+    network.add(MaxPooling2D(pool_size=(2, 2)))
 
     network.add(Conv2D(64, 3, padding='same', data_format='channels_first'))
     network.add(Conv2D(64, 3, padding='same', data_format='channels_first'))
 
-    network.add(MaxPooling2D(pool_size=(2,2)))
+    network.add(MaxPooling2D(pool_size=(2, 2)))
 
     network.add(Conv2D(128, 3, padding='same', data_format='channels_first'))
 
@@ -189,8 +179,8 @@ if __name__ == '__main__':
     network.add(Dense(num_classes, activation='sigmoid'))
 
     network.compile(optimizer='adam',
-        loss='binary_crossentropy',
-        metrics=['accuracy'])
+                    loss='binary_crossentropy',
+                    metrics=['accuracy'])
 
     # Class labels should start from 0
     print('Training the CNN Model...')
@@ -199,22 +189,40 @@ if __name__ == '__main__':
     y_val_cat = to_categorical(y_val)
     y_test_cat = to_categorical(y_test)
 
-    class_weights = {0:1, 1:1}
+    class_weights = {0: .2, 1: .8}
 
-    with tf.device('/gpu:0'):
-        network.fit(X_train, y_train_cat,
-                    validation_data=(X_val, y_val_cat),
-                    epochs=num_epochs,
-                    class_weight=class_weights)
+    network.fit(X_train, y_train_cat,
+                validation_data=(X_val, y_val_cat),
+                epochs=num_epochs,
+                class_weight=class_weights)
 
-    #del X_train
-    #del X_val
+    network.save('cnn_p300.h5')
+    network.save_weights('weights_cnn_p300.h5')
 
-    network.save('cnn_p300_1-12.h5')
-    network.save_weights('weights_cnn_p300_1-12.h5')
+    print(network.evaluate(X_test, y_test_cat))
 
-    with tf.device('/gpu:0'):
-        print(network.evaluate(X_test,y_test_cat))
+    predict = network.predict_classes(X_test)
 
+    fpr, tpr, threshold = roc_curve(y_test, predict)
+    area = auc(fpr, tpr)
+
+    print('\n')
+    print'balancing:{0}'.format(class_weights)
+    print'y_test = {0}'.format(y_train.sum())
+    print'predict = {0}'.format(predict.sum())
+    print'auc = {0}'.format(area)
+
+    plt.figure()
+    lw = 2
+    plt.plot(fpr[2], tpr[2], color='darkorange',
+             lw=lw, label='ROC curve (area = %0.4f)' % area)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
 
     print('Done!')
